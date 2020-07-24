e424f3298e55df1901006a31dda6f338bbada315
swatjoker <iswatxjokeri@gmail.com>
2020-07-23 23:59:22 -0400

    all done

--- W3D5/Exercise_1.rb
+++ W3D5/Exercise_1.rb
@@ -20,3 +20,48 @@ class Stack
       @store_stack[-1]
     end
 end
+
+class Queue
+    def initialize
+        @store_stack = []
+    end
+
+    def enqueue(el)
+        @store_stack.push(el)
+    end
+
+    def dequeue
+        @store_stack.unshift(el)
+    end
+
+    def peek
+        @store_stack[0]
+    end
+end
+
+class Map
+    attr_reader :my_map
+    def initialize
+        my_map = []
+    end
+
+    def set(key, value)
+        @my_map << [key, value]
+    end
+
+    def get(key)
+        my_map.each do |pair|
+            return key if pair.include?(key)
+        end
+    end
+
+    def delete(key)
+        my_map.each do |pair|
+            pair.shift if pair.include?(key)
+        end
+    end
+
+    def show
+        
+    end
+end
